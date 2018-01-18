f794d957cfc881a084dcf78add5fc193ede2f127
Dylan <dquesada186@gmail.com>
2018-01-18 09:56:13 -0600

    Added Node class with left and right Node pointers.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int newData)
+        {
+            data = newData;
+        }
+    }
+}
